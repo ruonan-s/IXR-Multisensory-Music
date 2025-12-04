8131ce218b013651c5d2e346ea12c49e6e61f20f
park-alivi <park.alivi@gmail.com>
2025-12-04 04:30:54 -0500

    skybox..

--- Assets/SkyboxFromVideo.cs
+++ Assets/SkyboxFromVideo.cs
@@ -1,45 +1,49 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Video;
 
 public class SkyboxFromVideo : MonoBehaviour
-
 {
-    public VideoPlayer mainVideo;          // 메시에서 재생되는 원본
-    public VideoPlayer colorSamplerVideo;  // RenderTexture 출력용
-    public RenderTexture videoRT;          // 색 뽑는 용도 작은 RT
+    public VideoPlayer mainVideo;          // 화면에 재생되는 원본 비디오
+    public VideoPlayer colorSamplerVideo;  // 색 추출용 비디오
+    public RenderTexture videoRT;          // 색 샘플링용 RenderTexture
 
-    public float colorLerpSpeed = 2f;      // 색이 부드럽게 변하는 속도
+    public float colorLerpSpeed = 2f;
 
     private Material skyMat;
 
     void Start()
     {
-        // Procedural Skybox 가져오기
+        // Skybox 머티리얼 가져오기
         skyMat = RenderSettings.skybox;
+
+        // 두 비디오 시작 위치를 0으로 맞추고 동시에 재생
+        mainVideo.time = 0;
+        colorSamplerVideo.time = 0;
+
+        mainVideo.Play();
+        colorSamplerVideo.Play();
     }
 
     void Update()
     {
-        // 두 VideoPlayer 시간 동기화
-        if (mainVideo && colorSamplerVideo)
-            colorSamplerVideo.time = mainVideo.time;
-
-        // RenderTexture에서 마지막 픽셀의 색 가져오기
+        // RenderTexture에서 픽셀 색 읽기
         Color c = GetLastPixelColor(videoRT);
 
-        // Sky Tint + Ground Color 업데이트
-        skyMat.SetColor("_SkyTint", Color.Lerp(skyMat.GetColor("_SkyTint"), c, Time.deltaTime * colorLerpSpeed));
-        skyMat.SetColor("_GroundColor", Color.Lerp(skyMat.GetColor("_GroundColor"), c, Time.deltaTime * colorLerpSpeed));
+        // Skybox 색 업데이트 (부드럽게)
+        skyMat.SetColor("_SkyTint",
+            Color.Lerp(skyMat.GetColor("_SkyTint"), c, Time.deltaTime * colorLerpSpeed));
 
-        // 스카이박스 즉시 업데이트 적용
+        skyMat.SetColor("_GroundColor",
+            Color.Lerp(skyMat.GetColor("_GroundColor"), c, Time.deltaTime * colorLerpSpeed));
+
+        // GI 업데이트
         DynamicGI.UpdateEnvironment();
     }
 
-    // 작은 텍스처에서 마지막 픽셀 색 읽기
+    // RenderTexture의 마지막 픽셀 색 추출
     Color GetLastPixelColor(RenderTexture rt)
     {
-        if (rt == null) return Color.black;
+        if (rt == null) return Color.yellow;
 
         RenderTexture.active = rt;
         Texture2D temp = new Texture2D(rt.width, rt.height, TextureFormat.RGB24, false);
@@ -47,7 +51,6 @@ public class SkyboxFromVideo : MonoBehaviour
         temp.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
         temp.Apply();
 
-        // 마지막 픽셀 색
         Color lastPixel = temp.GetPixel(rt.width - 1, rt.height - 1);
 
         RenderTexture.active = null;
